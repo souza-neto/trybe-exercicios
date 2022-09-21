9b169c422601348346623d50e4fbbaa0de70e95b
¨Souza <snetor@outlook.com>
2022-09-21 15:30:09 -0300

    Resolução exercicio-01

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/4.1-introducao-javascript/exercicio-01/scripts.js
@@ -0,0 +1,10 @@
+const myName = "Souza Neto";
+const birthCity = "Pau dos Ferros";
+let birthYear = 1998;
+birthYear = 2030;
+birthCity = "Mossoró";
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+// Na linha 5, está retornando o erro pois não é possivel alterar uma const
