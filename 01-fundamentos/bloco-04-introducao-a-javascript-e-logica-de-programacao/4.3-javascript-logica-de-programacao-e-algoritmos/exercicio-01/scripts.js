601c93f554d1e9c392ee8df8fff3a81353099ef4
¨Souza <snetor@outlook.com>
2022-09-25 07:17:15 -0300

    Resolução exercicio-4.3

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/4.3-javascript-logica-de-programacao-e-algoritmos/exercicio-01/scripts.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/4.3-javascript-logica-de-programacao-e-algoritmos/exercicio-01/scripts.js
@@ -5,6 +5,8 @@ for (let contFat = 1; contFat < fat; contFat += 1) {
     resultado = resultado * contFat;
 } console.log(resultado);
 
+//-----------------------------------------------------------
+
 let word = "tryber";
 let reverse = "";
 
@@ -12,3 +14,32 @@ for (let index = word.length - 1; index >= 0; index -=1) {
     reverse += word[index];    
 } console.log(reverse);
 
+//------------------------------------------------------------
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let menorPalavra = array[0];
+let maiorPalavra = array[0];
+
+for (let index = 0; index < array.length; index += 1){
+
+    for (let indexDentro = 0; indexDentro < array[index].length - 1; indexDentro += 1){ if (menorPalavra.length > array[index].length){
+        menorPalavra = array[index];
+    }
+    if (maiorPalavra.length < array[index].length){
+        maiorPalavra = array[index];
+    }
+    }
+}
+console.log(menorPalavra);
+console.log(maiorPalavra); 
+
+//-------------------------------------------------------------
+
+let maiorPrimo = 0
+
+for (let index = 2; index <= 50 ; index += 1) {
+    if (index % 2 != 0){
+        maiorPrimo = index
+    }
+    
+} console.log(maiorPrimo);
